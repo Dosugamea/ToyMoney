fc69fe42c28b448bbf0604ad32cfe60e17974140
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-07-19 04:16:55 +0900

    Now you can add admin more easily (3)

--- routers/database.py
+++ routers/database.py
@@ -4,6 +4,11 @@ from .models import Base
 
 # Database setting
 SALT = "UNSAFE_SECRET_KEY"
+DB_TYPE = 'sqlite'
+USERNAME = ''
+PASSWORD = ''
+HOST_IP = ''
+DB_NAME = 'TOYMONEY'
 ECHO = False
 
 if DB_TYPE == 'mysql':
@@ -15,12 +20,12 @@ if DB_TYPE == 'mysql':
     )
 else:
     DATABASE = f'sqlite:///{DB_NAME}.sqlite3'
-engine = create_engine(
-    DATABASE,
-    encoding="utf-8",
-    connect_args={"check_same_thread": False},
-    echo=ECHO
-)
+    engine = create_engine(
+        DATABASE,
+        encoding="utf-8",
+        connect_args={"check_same_thread": False},
+        echo=ECHO
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(engine)
